be0ec6e7d76a2bd3ba6076beba35940c0bb1a798
bhavin027 <bhavinghoghari27@gmail.com>
2020-11-10 19:34:09 +0530

    Created a project with basic java configuration(No XML)

---
+++ src/main/java/org/bhavin/springsecurity/demo/controller/DemoController.java
@@ -0,0 +1,14 @@
+package org.bhavin.springsecurity.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+	
+	@GetMapping("/")
+	public String showHomePage() {
+		return "home";
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<title>Spring Security Demo</title>
+</head>
+<body>
+	<h2>Welcome to the Spring Security Demo</h2>
+	<hr>
+	<p>Let's create security login page</p>
+</body>
+
+</html>
