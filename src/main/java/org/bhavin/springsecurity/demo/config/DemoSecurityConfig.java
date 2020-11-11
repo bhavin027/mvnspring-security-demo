442e7b125b7ebae9f74c7e64648d2f63007999d2
bhavin027 <bhavinghoghari27@gmail.com>
2020-11-11 17:55:30 +0530

    Implemented Spring Security Config(Default login page of Spring framework)

---
+++ src/main/java/org/bhavin/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,32 @@
+package org.bhavin.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		// add our users for in memory authentication
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+		
+		auth.inMemoryAuthentication()
+			.withUser(users.username("rooney").password("test123").roles("EMPLOYEE"))
+			.withUser(users.username("saf").password("test123").roles("MANAGER"))
+			.withUser(users.username("glazers").password("test123").roles("ADMIN"));
+		
+	}
+	
+	
+
+}
